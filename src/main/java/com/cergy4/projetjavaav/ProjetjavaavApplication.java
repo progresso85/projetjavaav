6868552b78bb5903ee7f5e9c853756395ff83366
progresso85 <martin.heuripeau@gmail.com>
2021-11-23 16:38:38 +0100

    Initial commit

---
+++ src/main/java/com/cergy4/projetjavaav/ProjetjavaavApplication.java
@@ -0,0 +1,13 @@
+package com.cergy4.projetjavaav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetjavaavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetjavaavApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cergy4/projetjavaav/ProjetjavaavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cergy4.projetjavaav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetjavaavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
