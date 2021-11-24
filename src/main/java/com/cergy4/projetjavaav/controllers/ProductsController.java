c8d08109bee50957f45d8fcf35b5f90157b77fc0
Raykutan <56077575+Raykutan@users.noreply.github.com>
2021-11-24 11:06:57 +0100

    route POST

--- src/main/java/com/cergy4/projetjavaav/controllers/ProductsController.java
+++ src/main/java/com/cergy4/projetjavaav/controllers/ProductsController.java
@@ -1,8 +1,15 @@
 package com.cergy4.projetjavaav.controllers;
 
+import com.cergy4.projetjavaav.models.Product;
 import com.cergy4.projetjavaav.services.ProductsDao;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/products")
@@ -13,4 +20,18 @@ public class ProductsController {
     public ProductsController(ProductsDao productsDao) {
         this.productsDao = productsDao;
     }
+
+    @PostMapping("")
+    public ResponseEntity<Object> postProduct(@RequestBody Product product) {
+        product.setCreatedAt(new Date());
+
+        String error = productsDao.checkValidity(product);
+        if (error != null)
+            return ResponseEntity.badRequest().body(error);
+
+        productsDao.add(product);
+
+        return ResponseEntity.created(URI.create("/products/"+ product.getId())).body(product);
+    }
+
 }
--- src/main/java/com/cergy4/projetjavaav/services/ProductsDao.java
+++ src/main/java/com/cergy4/projetjavaav/services/ProductsDao.java
@@ -1,9 +1,46 @@
 package com.cergy4.projetjavaav.services;
 
-import org.springframework.stereotype.Service;
+import com.cergy4.projetjavaav.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
-@Service
+@Repository
 public class ProductsDao {
 
+    private final JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    public ProductsDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public String checkValidity(Product product) {
+        if (product.getCreatedAt() == null)
+            return "missing field createdAt";
+        if (product.getName() == null)
+            return "missing field name";
+        if (product.getRating() > 10 || product.getRating() < 0)
+            return "rating must me between 0 and 10";
+        if (product.getType() == null)
+            return "missing field type";
+        if (product.getCategoryId() == 0)
+            return "missing field categoryId";
+
+        return null;
+    }
+
+    public void add(Product product) {
+        String sql = "INSERT INTO products (type, rating, name, createdAt, categoryId)" +
+                "VALUES (?, ?, ?, ?, ?);";
+        jdbcTemplate.update(sql,
+                product.getType(), product.getRating(), product.getName(), product.getCreatedAt(), product.getCategoryId());
+
+        Integer id = jdbcTemplate.query("SELECT LAST_INSERT_ID();",
+                rs -> { rs.next(); return rs.getInt(1);});
+
+        assert id != null;
+        product.setId(id);
+
+    }
 }
