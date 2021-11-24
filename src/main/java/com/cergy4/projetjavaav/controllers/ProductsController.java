5a2e44be0195d3f9a9570e502c9540685849c36d
Raykutan <56077575+Raykutan@users.noreply.github.com>
2021-11-24 11:47:35 +0100

    route put

--- src/main/java/com/cergy4/projetjavaav/controllers/ProductsController.java
+++ src/main/java/com/cergy4/projetjavaav/controllers/ProductsController.java
@@ -3,11 +3,9 @@ package com.cergy4.projetjavaav.controllers;
 
 import com.cergy4.projetjavaav.models.Product;
 import com.cergy4.projetjavaav.services.ProductsDao;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
 
 import java.net.URI;
 import java.util.Date;
@@ -30,8 +28,6 @@ public class ProductsController {
     }
 
 
-
-
     @PostMapping("")
     public ResponseEntity<Object> postProduct(@RequestBody Product product) {
         product.setCreatedAt(new Date());
@@ -44,6 +40,22 @@ public class ProductsController {
 
         return ResponseEntity.created(URI.create("/products/"+ product.getId())).body(product);
     }
+
+   @PutMapping("/{id}")
+   public ResponseEntity<Object> updateProduct(@PathVariable int id, @RequestBody Product product) {
+       String error = productsDao.checkValidity(product);
+       if (error != null)
+           return ResponseEntity.badRequest().body(error);
+
+       Product updatedProduct = productsDao.update(id, product);
+
+       if (updatedProduct == null) {
+           return ResponseEntity.notFound().build();
+       }
+       else {
+           return ResponseEntity.ok(updatedProduct);
+       }
+   }
 
 
 }
--- src/main/java/com/cergy4/projetjavaav/services/ProductsDao.java
+++ src/main/java/com/cergy4/projetjavaav/services/ProductsDao.java
@@ -1,12 +1,8 @@
 package com.cergy4.projetjavaav.services;
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.cergy4.projetjavaav.models.Product;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -28,8 +24,6 @@ public class ProductsDao {
 
 
     public String checkValidity(Product product) {
-        if (product.getCreatedAt() == null)
-            return "missing field createdAt";
         if (product.getName() == null)
             return "missing field name";
         if (product.getRating() > 10 || product.getRating() < 0)
@@ -55,5 +49,20 @@ public class ProductsDao {
         product.setId(id);
 
 
+    }
+
+    public Product update(int id, Product product) {
+        String sql = "UPDATE products SET name = ?, rating = ?, type = ?, categoryId = ? " +
+                "WHERE id = ?;";
+
+        int result = jdbcTemplate.update(sql,
+                product.getName(), product.getRating(), product.getType(), product.getCategoryId(), id);
+
+        if (result != 1) {
+            return null;
+        }
+
+        return jdbcTemplate.query("SELECT * FROM products WHERE id = ?", BeanPropertyRowMapper.newInstance(Product.class),
+                id).get(0);
     }
 }
