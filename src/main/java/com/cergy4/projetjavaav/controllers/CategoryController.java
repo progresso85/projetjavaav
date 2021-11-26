85d74c17c7f2bef9b727ab3ca2ab2ddd19cacffe
Brian <nairb725@gmail.com>
2021-11-26 12:18:41 +0100

    authors and api doc

--- src/main/java/com/cergy4/projetjavaav/controllers/CategoryController.java
+++ src/main/java/com/cergy4/projetjavaav/controllers/CategoryController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/categories")
 public class CategoryController {
 
--- src/main/java/com/cergy4/projetjavaav/controllers/ProductsController.java
+++ src/main/java/com/cergy4/projetjavaav/controllers/ProductsController.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.Optional;
 
 
-@Controller
+@RestController
 @RequestMapping("/products")
 public class ProductsController {
 
